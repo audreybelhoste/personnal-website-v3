f1dda4c2b7c3415677b169cf0db1feeb31b996de
Audrey <audrey.belhoste@gmail.com>
2022-03-31 17:49:49 +0200

    change colors + style list of projects

--- wp-content/themes/portfolio/front-page.php
+++ wp-content/themes/portfolio/front-page.php
@@ -23,13 +23,9 @@ get_header(); ?>
           <div>
             <h1 class="presentation__title">Bonjour, je suis <span class="presentation__name">Audrey Belhoste</span></h1>
             <p class="presentation__text">Je suis <span class="presentation__underline">développeuse web</span>, intéressée à la fois par le <span class="presentation__bold">front-end</span> et le <span class="presentation__bold">back-end</span>.</p>
-            <p class="presentation__text">Je vous présente ici <span class="presentation__italic">mon parcours</span>.</p>
-            <a class="presentation__cv" href="./files/cv_audrey_belhoste.pdf" download target="_blank">
-              <img src="img/badgecv.svg">
-            </a>
           </div>
           <img class="presentation__container__oval" src="<?= get_template_directory_uri(); ?>/images/oval.svg" alt="" data-depth="0.9">
-          <img class="presentation__container__sparkle" src="img/sparkle.svg" alt="" data-depth="0.1">
+          <img class="presentation__container__sparkle" src="<?= get_template_directory_uri(); ?>/images/sparkle.svg" alt="" data-depth="0.1">
         </div>
           <ul class="presentation__competences">
             <li class="presentation__competences__item">HTML</li>
@@ -43,100 +39,125 @@ get_header(); ?>
           </ul>
         </div>
       </section>
-      <section id="projects" class="section projects right">
+      <section id="projects" class="section project right">
         <h2 class="mainTitle">Projets</h2>
           <div class="section__content">
-            <a href="pages/entrepriseProject.html" class="projects__item">
-              <h3 class="projects__item__title"><span class="projects__item__title__number">01</span>Projet en entreprise</h3>
-              <div class="projects__item__img">
-                <img src="img/learning-mate.png" alt="Capture d'écran page de connexion Learning-mate">
+            <div class="project__item">
+            <h3 class="project__item__title"><span class="project__item__title__number">01</span>OhMyFood</h3>
+            <div class="project__item__container">
+              <div class="project__item__img">
+                <img src="<?= get_template_directory_uri(); ?>/images/ohmyfood.png" alt="Capture d'écran OhMyFood">
               </div>
-              <div class="projects__item__description">
-                <p>Learning-mate : plateforme d’accompagment pédagogique pour les stagiaires Ibep Formation.</p>
-                <span class="btn-primary">Voir plus</span>
-              </div>
-            </a>
-
-            <a href="pages/schoolProjects.html" class="projects__item">
-              <h3 class="projects__item__title"><span class="projects__item__title__number">02</span>Projets de formation</h3>
-              <div class="projects__item__img">
-                <img src="img/sportsee.png" alt="Capture d'écran projet SportSee">
-              </div>
-              <div class="projects__item__description">
-                <p>Sélection de différents projets réalisées durant ma formation OpenClassrooms et de challenges issus du site Frontend Mentor.</p>
-                <span class="btn-primary">Voir plus</span>
-              </div>
-            </a>
-
-            <a href="pages/personnalProject.html" class="projects__item">
-              <h3 class="projects__item__title"><span class="projects__item__title__number">03</span>Projet personnel</h3>
-              <div class="projects__item__img">
-                <img src="img/quiveutquoi.png" alt="Capture d'écran Qui veut quoi">
-              </div>
-              <div class="projects__item__description">
-                <p>Qui veut quoi ? Création et partage de listes de cadeaux.</p>
-                <span class="btn-primary">Voir plus</span>
-              </div>
-            </a>
-        </section>
-        <section id="experiences" class="section experiences right">
-          <h2 class="mainTitle">Expériences</h2>
-          <div class="section__content">
-            <div class="experiences__item">
-              <p class="experiences__item__date experiences__item__date--double"><span>2021</span><span>/</span><span>2022</span></p>
-              <div class="experiences__item__details experiences__item__details--first">
-                <p><span class="experiences__item__details--bold">Développeur d'application front-end</span></p>
-                <p>OpenClassrooms</p>
-              </div>
-              <div class="experiences__item__details experiences__item__details--second">
-                <p><span class="experiences__item__details--bold">Alternance</span></p>
-                <p>IBEP Formation</p>
-              </div>
-            </div>
-            <div class="experiences__item">
-              <p class="experiences__item__date"><span>20</span><span>20</span></p>
-              <div class="experiences__item__details experiences__item__details--first">
-                <p><span class="experiences__item__details--bold">Développeur web et web mobile</span></p>
-                <p>Eni Ecole Informatique</p>
-              </div>
-              <div class="experiences__item__details experiences__item__details--second">
-                <p><span class="experiences__item__details--bold">Stage</span></p>
-                <p>IBEP Formation</p>
-              </div>
-            </div>
-            <div class="experiences__item">
-              <p class="experiences__item__date experiences__item__date--double"><span>2016</span><span>/</span><span>2019</span></p>
-              <div class="experiences__item__details">
-                <p><span class="experiences__item__details--bold">Professeure des écoles</span></p>
-              </div>
-            </div>
-            <div class="experiences__item">
-              <p class="experiences__item__date experiences__item__date--double"><span>2015</span><span>/</span><span>2017</span></p>
-              <div class="experiences__item__details">
-                <p><span class="experiences__item__details--bold">Master Métiers de l’enseignement, de l’éducation et de la formation</span></p>
-                <p>Université de Caen</p>
-              </div>
-            </div>
-            <div class="experiences__item">
-              <p class="experiences__item__date"><span>20</span><span>15</span></p>
-              <div class="experiences__item__details experiences__item__details--first">
-                <p><span class="experiences__item__details--bold">Licence 3 Sciences de l'éducation</span></p>
-                <p>Université de Caen</p>
-              </div>
-              <div class="experiences__item__details experiences__item__details--second">
-                <p><span class="experiences__item__details--bold">Mission de service civique</span></p>
-                <p>AFEV Caen</p>
-              </div>
-            </div>
-            <div class="experiences__item">
-              <p class="experiences__item__date"><span>20</span><span>14</span></p>
-              <div class="experiences__item__details">
-                <p><span class="experiences__item__details--bold">DUT Information-Communication</span></p>
-                <p>Université de Caen</p>
+              <div class="project__item__informations">
+                <div class="project__item__details">
+                  <div>
+                    <p class="project__item__details__title">Année</p>
+                    <p class="project__item__details__text">2021</p>
+                  </div>
+                  <div>
+                    <p class="project__item__details__title">Technologies utilisées</p>
+                    <p class="project__item__details__text">HTML / Sass</p>
+                  </div>
+                </div>
+                <div class="project__item__description">
+                  <p class="project__item__description__title">Le besoin (fictif)</p>
+                  <p>Ohmyfood! est une jeune startup qui voudrait s'imposer sur le marché de la restauration. L'objectif est de développer un site 100% mobile qui répertorie les menus de restaurants gastronomiques. En plus des systèmes classiques de réservation, les clients pourront composer le menu de leur repas pour que les plats soient prêts à leur arrivée. Finis, les temps d'attente au restaurant ! </p>
+                  <div class="btnGroup">
+                    <a href="https://audreybelhoste.github.io/AudreyBelhoste_3_05022021/index.html" target="_blank" class="btn-primary">Accéder au site</a>
+                    <a href="https://github.com/audreybelhoste/AudreyBelhoste_3_05022021" target="_blank" class="btn-primary">Accéder au code</a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-      </section>
+
+          <div class="project__item">
+            <h3 class="project__item__title"><span class="project__item__title__number">02</span>SportSee</h3>
+            <div class="project__item__container">
+              <div class="project__item__img">
+                <img src="<?= get_template_directory_uri(); ?>/images/sportsee.png" alt="Capture d'écran OhMyFood">
+              </div>
+              <div class="project__item__informations">
+                <div class="project__item__details">
+                  <div>
+                    <p class="project__item__details__title">Année</p>
+                    <p class="project__item__details__text">2021</p>
+                  </div>
+                  <div>
+                    <p class="project__item__details__title">Technologies utilisées</p>
+                    <p class="project__item__details__text">React : utilisation de Recharts et Axios</p>
+                  </div>
+                </div>
+                <div class="project__item__description">
+                  <p class="project__item__description__title">Le besoin (fictif)</p>
+                  <p>SportSee est une startup dédiée au coaching sportif. En pleine croissance, l’entreprise va aujourd’hui lancer une nouvelle version de la page profil de l’utilisateur. Cette page va notamment permettre à l’utilisateur de suivre le nombre de sessions réalisées ainsi que le nombre de calories brûlées. </p>
+                  <div class="btnGroup">
+                    <a href="https://github.com/audreybelhoste/AudreyBelhoste_12_06012022" target="_blank" class="btn-primary">Accéder au code</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="project__item">
+            <h3 class="project__item__title"><span class="project__item__title__number">03</span>Qui veut quoi ?</h3>
+            <div class="project__item__container">
+              <div class="project__item__img">
+                <img src="<?= get_template_directory_uri(); ?>/images/quiveutquoi.png" alt="Capture d'écran Qui veut quoi ?">
+              </div>
+              <div class="project__item__informations">
+                <div class="project__item__details">
+                  <div>
+                    <p class="project__item__details__title">Année</p>
+                    <p class="project__item__details__text">2022</p>
+                  </div>
+                  <div>
+                    <p class="project__item__details__title">Technologies utilisées</p>
+                    <p class="project__item__details__text">A venir </p>
+                  </div>
+                </div>
+                <div class="project__item__description">
+                  <p class="project__item__description__title">Le besoin</p>
+                  <p>A venir</p>
+                  <p class="project__item__description__title">L'avancement du projet</p>
+                  <p>Les maquettes sont presques finalisées. Date de sortie prévue pour la première version : avant la fin du mois d'avril.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="project__item">
+          <h3 class="project__item__title"><span class="project__item__title__number">04</span>Learning-Mate</h3>
+            <div class="project__item__container">
+              <div class="project__item__img">
+                <img src="<?= get_template_directory_uri(); ?>/images/learning-mate.png" alt="Capture d'écran page de connexion Learning-mate">
+              </div>
+              <div class="project__item__informations">
+                <div class="project__item__details">
+                  <div>
+                    <p class="project__item__details__title">Année</p>
+                    <p class="project__item__details__text">2021-2022</p>
+                  </div>
+                  <div>
+                    <p class="project__item__details__title">Technologies utilisées</p>
+                    <p class="project__item__details__text">PHP 8 / Symfony 5 / Bootstrap / Sass / Javascript Vanilla </p>
+                  </div>
+                </div>
+                <div class="project__item__description">
+                  <p class="project__item__description__title">Le besoin</p>
+                  <p>Permettre au stagiaire de voir son avancement dans sa prestation, d’avoir une vision de son parcours, l’accompagner au mieux dans son apprentissage. C’est la volonté de l’IBEP qui a donc choisi d’internaliser sa plateforme de suivi pédagogique. 
+                    Le développement en interne permet de répondre au plus vite aux besoins des stagiaires et des formateurs et de proposer régulièrement de nouvelles fonctionnalités. </p>
+                  <p class="project__item__description__title">Mes réalisations durant ce projet</p>
+                  <ul>
+                    <li>Analyse de l’existant et recueil du besoin </li>
+                    <li>Rédaction du cahier des charges </li>
+                    <li>Réalisation des maquettes avec Adobe XD</li>
+                    <li>Développement front et back de certaines fonctionnalités</li>
+                    <li>Suivi du projet avec Notion</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       <section id="contact" class="section contact right">
         <h2 class="mainTitle">Contact</h2>
         <div class="section__content">
@@ -163,7 +184,7 @@ get_header(); ?>
           </form>
           <div class="contact__linkedin">
             <a href="https://www.linkedin.com/in/audrey-belhoste-096b8118a/" target="_blank">
-              <img src="img/linkedin.svg" alt="Me connacter sur Linkedin">
+              <img src="<?= get_template_directory_uri(); ?>/images/linkedin.svg" alt="Me connacter sur Linkedin">
             </a>
           </div>
         </div>
